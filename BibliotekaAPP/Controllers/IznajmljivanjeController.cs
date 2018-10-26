76feae019cdaabb7019136265ec35fc086345a86
0649002418 <zoran.radeski92@gmail.com>
2018-10-26 13:30:52 +0200

    Bibliotek commit.

---
+++ BibliotekaAPP/Controllers/IznajmljivanjeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BibliotekaAPP.Models;
+
+namespace BibliotekaAPP.Controllers
+{
+    public class IznajmljivanjeController : Controller
+    {
+        private BibliotekaDBEntities db = new BibliotekaDBEntities();
+
+        // GET: Iznajmljivanje
+        public ActionResult Index()
+        {
+            var iznajmljivanjes = db.Iznajmljivanjes.Include(i => i.Knjige).Include(i => i.Korisnici);
+            return View(iznajmljivanjes.ToList());
+        }
+
+        // GET: Iznajmljivanje/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Iznajmljivanje iznajmljivanje = db.Iznajmljivanjes.Find(id);
+            if (iznajmljivanje == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iznajmljivanje);
+        }
+
+        // GET: Iznajmljivanje/Create
+        public ActionResult Create()
+        {
+            ViewBag.KnjigaID = new SelectList(db.Knjiges, "KnjigaID", "Naslov");
+            ViewBag.KorisnikID = new SelectList(db.Korisnicis, "KorisnikID", "Ime");
+            return View();
+        }
+
+        // POST: Iznajmljivanje/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IznajmljivanjeID,KorisnikID,KnjigaID,DatumIznajmljivanja")] Iznajmljivanje iznajmljivanje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Iznajmljivanjes.Add(iznajmljivanje);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.KnjigaID = new SelectList(db.Knjiges, "KnjigaID", "Naslov", iznajmljivanje.KnjigaID);
+            ViewBag.KorisnikID = new SelectList(db.Korisnicis, "KorisnikID", "Ime", iznajmljivanje.KorisnikID);
+            return View(iznajmljivanje);
+        }
+
+        // GET: Iznajmljivanje/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Iznajmljivanje iznajmljivanje = db.Iznajmljivanjes.Find(id);
+            if (iznajmljivanje == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.KnjigaID = new SelectList(db.Knjiges, "KnjigaID", "Naslov", iznajmljivanje.KnjigaID);
+            ViewBag.KorisnikID = new SelectList(db.Korisnicis, "KorisnikID", "Ime", iznajmljivanje.KorisnikID);
+            return View(iznajmljivanje);
+        }
+
+        // POST: Iznajmljivanje/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IznajmljivanjeID,KorisnikID,KnjigaID,DatumIznajmljivanja")] Iznajmljivanje iznajmljivanje)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(iznajmljivanje).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.KnjigaID = new SelectList(db.Knjiges, "KnjigaID", "Naslov", iznajmljivanje.KnjigaID);
+            ViewBag.KorisnikID = new SelectList(db.Korisnicis, "KorisnikID", "Ime", iznajmljivanje.KorisnikID);
+            return View(iznajmljivanje);
+        }
+
+        // GET: Iznajmljivanje/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Iznajmljivanje iznajmljivanje = db.Iznajmljivanjes.Find(id);
+            if (iznajmljivanje == null)
+            {
+                return HttpNotFound();
+            }
+            return View(iznajmljivanje);
+        }
+
+        // POST: Iznajmljivanje/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Iznajmljivanje iznajmljivanje = db.Iznajmljivanjes.Find(id);
+            db.Iznajmljivanjes.Remove(iznajmljivanje);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BibliotekaAPP/Controllers/KnjigeController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BibliotekaAPP.Models;
+
+namespace BibliotekaAPP.Controllers
+{
+    public class KnjigeController : Controller
+    {
+        private BibliotekaDBEntities db = new BibliotekaDBEntities();
+
+        // GET: Knjige
+        public ActionResult Index()
+        {
+            return View(db.Knjiges.ToList());
+        }
+
+        // GET: Knjige/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Knjige knjige = db.Knjiges.Find(id);
+            if (knjige == null)
+            {
+                return HttpNotFound();
+            }
+            return View(knjige);
+        }
+
+        // GET: Knjige/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Knjige/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KnjigaID,Naslov,Pisac,Kategorija")] Knjige knjige)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Knjiges.Add(knjige);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(knjige);
+        }
+
+        // GET: Knjige/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Knjige knjige = db.Knjiges.Find(id);
+            if (knjige == null)
+            {
+                return HttpNotFound();
+            }
+            return View(knjige);
+        }
+
+        // POST: Knjige/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "KnjigaID,Naslov,Pisac,Kategorija")] Knjige knjige)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(knjige).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(knjige);
+        }
+
+        // GET: Knjige/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Knjige knjige = db.Knjiges.Find(id);
+            if (knjige == null)
+            {
+                return HttpNotFound();
+            }
+            return View(knjige);
+        }
+
+        // POST: Knjige/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Knjige knjige = db.Knjiges.Find(id);
+            db.Knjiges.Remove(knjige);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
